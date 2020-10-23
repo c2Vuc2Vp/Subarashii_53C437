ad8d452851857ed1f6af6f9d0342c96334d1d61b
c2Vuc2Vp <73338752+c2Vuc2Vp@users.noreply.github.com>
2020-10-23 10:41:32 +0000

    Add files via upload

---
+++ reseau/hostpost/demarrerHostpost.bat
@@ -0,0 +1,6 @@
+netsh wlan start hostednetwork
+@ECHO OFF
+cls
+echo.
+echo Hostpost lance avec succes :
+ping 127.0.0.1 -n 2 > nul
---
+++ reseau/hostpost/stopHostpost.bat
@@ -0,0 +1,6 @@
+netsh wlan stop hostednetwork
+@ECHO OFF
+cls
+echo.
+echo Hostpost stopper avec succes :
+ping 127.0.0.1 -n 2 > nul
