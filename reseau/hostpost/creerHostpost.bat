e3426d806fbbabb7aa130cf1ace5f104a0c8e50a
c2Vuc2Vp <73338752+c2Vuc2Vp@users.noreply.github.com>
2020-10-23 10:40:50 +0000

    Create creerHostpost.bat

---
+++ reseau/hostpost/creerHostpost.bat
@@ -0,0 +1,9 @@
+
+netsh wlan set hostednetwork mode=allow ssid=53C437 key="53C437"
+@ECHO OFF
+cls
+echo.
+echo Hostpost cree avec succes :
+echo            SSID = 53C437
+echo            MDP  = 53C437
+ping 127.0.0.1 -n 2 > nul
