b1cca376f2c9c075ace5bc5ff9f74c8c58749cbc
c2Vuc2Vp <73338752+c2Vuc2Vp@users.noreply.github.com>
2020-10-24 11:21:54 +0000

    Add files via upload

--- reseau/wifi/mdp.bat
+++ reseau/wifi/mdp.bat
@@ -5,5 +5,13 @@ echo Trouver le mot de passe dans le fichier qui se
 echo situe dans le repertoire suivant :
 echo reseau/wifi/mot_de_passe.txt
 echo.
+echo        ##########################################################
+echo        #                                                        #
+echo        #    Trouver le mot de passe dans le fichier qui se      #
+echo        #         situe dans le repertoire suivant :             #
+echo        #           reseau/wifi/mot_de_passe.txt                 #
+echo        #                                                        #
+echo        #                                                        #
+echo        ##########################################################
+echo.
 ping 127.0.0.1 -n 2 > nul
-
