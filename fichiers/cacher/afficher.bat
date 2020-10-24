2adcb1c864f133208d406a3e8f633357ea02c045
c2Vuc2Vp <73338752+c2Vuc2Vp@users.noreply.github.com>
2020-10-24 11:23:10 +0000

    Add files via upload

--- fichiers/cacher/afficher.bat
+++ fichiers/cacher/afficher.bat
@@ -2,8 +2,15 @@ attrib -h -s fichiers/cacher/"Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309
 ren "fichiers/cacher/Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" 53C437
 @ECHO OFF
 cls
-echo 53C437 debloque
+echo.
+echo        ##########################################################
+echo        #                                                        #
+echo        #                                                        #
+echo        #         fichiers 53C437 est maintenant                 #
+echo        #                   visible                              #
+echo        #                                                        #
+echo        #                                                        #
+echo        ##########################################################
 echo.
 ping 127.0.0.1 -n 2 > nul
-
 
--- fichiers/cacher/cacher.bat
+++ fichiers/cacher/cacher.bat
@@ -2,6 +2,14 @@ ren fichiers/cacher/53C437 "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}
 attrib +h +s fichiers/cacher/"Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 @ECHO OFF
 cls
-echo 53C437 verouille
+echo.
+echo        ##########################################################
+echo        #                                                        #
+echo        #                                                        #
+echo        #         fichiers 53C437 est maintenant                 #
+echo        #                   invisible                            #
+echo        #                                                        #
+echo        #                                                        #
+echo        ##########################################################
 echo.
 ping 127.0.0.1 -n 2 > nul
